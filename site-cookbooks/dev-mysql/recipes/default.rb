2b40fbf953171fdf49d01a0177c78747c7c3892f
Nick C <nick.chernyshev@gmail.com>
2016-07-18 19:06:31 +0200

    memo for nodejs

--- site-cookbooks/dev-mysql/recipes/default.rb
+++ site-cookbooks/dev-mysql/recipes/default.rb
@@ -1,2 +1,6 @@
 package 'mysql-client-5.6'
 package 'libmysqlclient-dev'
+
+# create new clean recipe to install nodejs
+# curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+# sudo apt-get install -y nodejs
