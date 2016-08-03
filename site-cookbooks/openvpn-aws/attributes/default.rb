c6cc2eb31718791cb7a44b42ed9ff763e9143dcd
Nick C <nick.chernyshev@gmail.com>
2016-08-03 13:46:54 +0200

    fix openvpn server key generation

--- site-cookbooks/openvpn-aws/attributes/default.rb
+++ site-cookbooks/openvpn-aws/attributes/default.rb
@@ -1,4 +1,19 @@
 default['openvpn']['reconfigure'] = false
+default['openvpn']['reconfigure_vars'] = false
+
+default['openvpn']['key']['easy_rsa'] = "/etc/openvpn/easy-rsa" #'EASY_RSA'
+default['openvpn']['key']['openssl'] = "openssl" #'OPENSSL'
+default['openvpn']['key']['pkcs11_tool'] = "pkcs11-tool" #PKCS11TOOL
+default['openvpn']['key']['grep'] = "grep" #GREP
+default['openvpn']['key']['key_config'] = "#{node['openvpn']['key']['easy_rsa']}/openssl-1.0.0.cnf" #KEY_CONFIG
+default['openvpn']['key']['key_dir'] = "#{node['openvpn']['key']['easy_rsa']}/keys" #KEY_DIR
+default['openvpn']['key']['pkcs11_module_path'] = "dummy" #PKCS11_MODULE_PATH
+default['openvpn']['key']['pkcs11_pin'] = "dummy" #PKCS11_PIN
+
+default['openvpn']['key']['ca_expire'] = "3650"
+default['openvpn']['key']['expire'] = "3650"
+default['openvpn']['key']['size'] = "2048"
+
 default['openvpn']['key']['country'] = "US"
 default['openvpn']['key']['region'] = "CA"
 default['openvpn']['key']['city'] = "SanFrancisco"
--- site-cookbooks/openvpn-aws/recipes/default.rb
+++ site-cookbooks/openvpn-aws/recipes/default.rb
@@ -6,7 +6,7 @@ if node[:openvpn][:reconfigure]
     owner "root"
     group "root"
     mode "0644"
-    source "server.conf"
+    source "server.conf.erb"
   end
 
   template "/etc/sysctl.conf" do
@@ -52,24 +52,22 @@ bash "ufw open port and reload" do
 end
 
 execute "generate the Diffie-Hellman parameters" do
-  # user "root"
   command "openssl dhparam -out /etc/openvpn/dh2048.pem 2048"
   not_if { ::File.exist?("/etc/openvpn/dh2048.pem") }
-  # action :nothing
 end
 
 execute "Configure and Build the Certificate Authority" do
-  # user "root"
   command "cp -r /usr/share/easy-rsa/ /etc/openvpn"
   not_if { ::Dir.exist?("/etc/openvpn/easy-rsa") }
-  # action :nothing
 end
 
-template "/etc/openvpn/easy-rsa/vars" do
-  owner "root"
-  group "root"
-  mode "0644"
-  source "vars.erb"
+if node[:openvpn][:reconfigure_vars]
+  template "/etc/openvpn/easy-rsa/vars" do
+    owner "root"
+    group "root"
+    mode "0644"
+    source "vars.erb"
+  end
 end
 
 directory '/etc/openvpn/easy-rsa/keys' do
@@ -78,29 +76,70 @@ directory '/etc/openvpn/easy-rsa/keys' do
   mode  '0755'
 end
 
-execute "Initialize the PKI (Public Key Infrastructure)" do
-  command "cd /etc/openvpn/easy-rsa"
-  command "source ./vars"
+# source ENV
+ENV['EASY_RSA'] = node['openvpn']['key']['easy_rsa']
+ENV['OPENSSL'] = node['openvpn']['key']['openssl']
+ENV['PKCS11TOOL'] = node['openvpn']['key']['pkcs11_tool']
+ENV['GREP'] = node['openvpn']['key']['grep']
+ENV['KEY_CONFIG'] = node['openvpn']['key']['key_config']
+ENV['KEY_DIR'] = node['openvpn']['key']['key_dir']
+ENV['PKCS11_MODULE_PATH'] = node['openvpn']['key']['pkcs11_module_path']
+ENV['PKCS11_PIN'] = node['openvpn']['key']['pkcs11_pin']
+ENV['CA_EXPIRE'] = node['openvpn']['key']['ca_expire']
+ENV['KEY_EXPIRE'] = node['openvpn']['key']['expire']
+ENV['KEY_SIZE'] = node['openvpn']['key']['size']
+ENV['KEY_COUNTRY'] = node['openvpn']['key']['country']
+ENV['KEY_PROVINCE'] = node['openvpn']['key']['region']
+ENV['KEY_CITY'] = node['openvpn']['key']['city']
+ENV['KEY_ORG'] = node['openvpn']['key']['org']
+ENV['KEY_EMAIL'] = node['openvpn']['key']['email']
+ENV['KEY_OU'] = node['openvpn']['key']['org_unit']
+ENV['KEY_NAME'] = node['openvpn']['key']['name']
+
+execute "Cleaning previous keys" do
+  user "root"
   command "./clean-all"
-  command "./build-ca" # fails here!!!
+  cwd "/etc/openvpn/easy-rsa"
+  not_if { ::File.exist?("/etc/openvpn/easy-rsa/keys/ca.crt") }
+end
+
+execute "Build CA" do
+  user "root"
+  command "./pkitool --initca"
+  cwd "/etc/openvpn/easy-rsa"
   not_if { ::File.exist?("/etc/openvpn/easy-rsa/keys/ca.crt") }
 end
 
 execute "Generate a Certificate and Key for the Server" do
-  command "cd /etc/openvpn/easy-rsa"
-  command "./build-key-server #{node[:openvpn][:key][:name]}" # fails here!!!
-  command "cp /etc/openvpn/easy-rsa/keys/{#{node[:openvpn][:key][:name]}.crt,#{node[:openvpn][:key][:name]}.key,ca.crt} /etc/openvpn"
+  user "root"
+  command "./pkitool --server #{node[:openvpn][:key][:name]}"
+  cwd "/etc/openvpn/easy-rsa"
+  not_if { ::File.exist?("/etc/openvpn/easy-rsa/keys/#{node[:openvpn][:key][:name]}.crt") }
+end
+
+execute "Copy CA" do
+  user "root"
+  cwd "/etc/openvpn/easy-rsa/keys"
+  command "cp ca.crt /etc/openvpn"
+  not_if { ::File.exist?("/etc/openvpn/ca.crt") }
+end
+
+execute "Copy Server cert" do
+  user "root"
+  cwd "/etc/openvpn/easy-rsa/keys"
+  command "cp #{node[:openvpn][:key][:name]}.crt /etc/openvpn"
   not_if { ::File.exist?("/etc/openvpn/#{node[:openvpn][:key][:name]}.crt") }
 end
 
-service 'openvpn' do
-  action [:enable, :start]
-  # action :restart
+execute "Copy Server key" do
+  user "root"
+  cwd "/etc/openvpn/easy-rsa/keys"
+  command "cp #{node[:openvpn][:key][:name]}.key /etc/openvpn"
+  not_if { ::File.exist?("/etc/openvpn/#{node[:openvpn][:key][:name]}.key") }
 end
 
-# Aug  2 16:26:09 ip-10-1-0-25 pollinate[26243]: system was previously seeded at [2016-08-02 11:59:45.927983000 +0000]
-# Aug  2 16:26:09 ip-10-1-0-25 pollinate[26247]: To re-seed this system again, use the -r|--reseed option
-# Aug  2 16:26:11 ip-10-1-0-25 ovpn-server[27575]: Options error: --cert fails with 'server.crt': No such file or directory
-# Aug  2 16:26:11 ip-10-1-0-25 ovpn-server[27575]: Options error: --key fails with 'server.key': No such file or directory
-# Aug  2 16:26:11 ip-10-1-0-25 ovpn-server[27575]: Options error: Please correct these errors.
-# Aug  2 16:26:11 ip-10-1-0-25 ovpn-server[27575]: Use --help for more information.
+service 'openvpn' do
+  action [:enable, :restart]
+end
+
+# ./pkitool client1
